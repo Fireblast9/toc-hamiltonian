dc959a9b75f9379a710a160ae74341848cc8b3ae
Sasha Toscano <sasha.toscano97@gmail.com>
2025-05-11 17:47:41 +0200

    Fixed and updated gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
-from datetime import date
-from os import getenv
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configure CORS
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
-def read_root():
-    return "Hello World"
+async def read_root():
+    return {"Hello, world!"}
