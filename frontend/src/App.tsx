18539537168f213fccdff4a79cdc388ece7f3e65
Sasha Toscano <sasha.toscano97@gmail.com>
2025-05-23 15:46:58 +0200

    Started implementing input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,33 @@
-import { useCallback } from 'react';
-import {
-  ReactFlow,
-  Background,
-  Controls,
-  MiniMap,
-  addEdge,
-  useNodesState,
-  useEdgesState,
-  type OnConnect,
-} from '@xyflow/react';
-
-import '@xyflow/react/dist/style.css';
-
-import { initialNodes, nodeTypes } from './nodes';
-import { initialEdges, edgeTypes } from './edges';
+import { useState } from "react";
 
 export default function App() {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const onConnect: OnConnect = useCallback(
-    (connection) => setEdges((edges) => addEdge(connection, edges)),
-    [setEdges]
-  );
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  async function fetchOutput() {
+    const response = await fetch(`http://localhost:8000/solve/${input}`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    setOutput(await response.json());
+  }
+
+  console.log("input: ", input);
 
   return (
-    <ReactFlow
-      nodes={nodes}
-      nodeTypes={nodeTypes}
-      onNodesChange={onNodesChange}
-      edges={edges}
-      edgeTypes={edgeTypes}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      fitView
-    >
-      <Background />
-      <MiniMap />
-      <Controls />
-    </ReactFlow>
+    <div className="App">
+      <h1>SAT Solver</h1>
+      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
+
+      <button
+        onClick={() => {
+          fetchOutput();
+        }}
+      >
+        Solve
+      </button>
+
+      <p>{output}</p>
+    </div>
   );
 }
